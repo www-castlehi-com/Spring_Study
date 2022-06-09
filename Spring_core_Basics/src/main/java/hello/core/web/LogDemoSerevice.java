851aaaf24208a7265e295a2d8146a4619d04086f
moong2 <www.castlehi@gmail.com>
2022-06-09 15:44:27 +0900

    request 스코프 예제 만들기

--- Spring_core_Basics/src/main/java/hello/core/web/LogDemoSerevice.java
+++ Spring_core_Basics/src/main/java/hello/core/web/LogDemoSerevice.java
@@ -1,2 +1,16 @@
-package hello.core.web;public class LogDemoSerevice {
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoSerevice {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
 }
