e39606787bb8fb59c3750c6cf3c9731a6628ecfb
moong2 <www.castlehi@gmail.com>
2023-08-23 20:52:14 +0900

    fix: version change

---
+++ spring_introduction/src/main/java/spring_study/spring_introduction/SpringIntroductionApplication.java
@@ -0,0 +1,13 @@
+package spring_study.spring_introduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntroductionApplication.class, args);
+	}
+
+}
---
+++ spring_introduction/src/test/java/spring_study/spring_introduction/SpringIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_study.spring_introduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
