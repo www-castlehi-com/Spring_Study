cb2a66db016621eb7580e58f65ea7acdfa6bf439
moong2 <www.castlehi@gmail.com>
2024-04-12 13:19:33 +0900

    섹션2 <인증 프로세스> 1. 폼 인증 - formLogin()

--- Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -1,15 +1,21 @@
 package io.security.springsecuritymaster;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @EnableWebSecurity
 @Configuration
@@ -19,7 +25,29 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults());
+                .formLogin(form -> form
+//                        .loginPage("/loginPage")
+                        .loginProcessingUrl("/loginProc")
+                        .defaultSuccessUrl("/", true)
+                        .failureUrl("/failed")
+                        .usernameParameter("userId")
+                        .passwordParameter("passwd")
+                        .successHandler(new AuthenticationSuccessHandler() {
+                            @Override
+                            public void onAuthenticationSuccess(HttpServletRequest request,
+                                                                HttpServletResponse response,
+                                                                Authentication authentication)
+                                    throws IOException, ServletException {
+                                System.out.println("authentication : " + authentication);
+                                response.sendRedirect("/home");
+                            }
+                        })
+                        .failureHandler(((request, response, exception) -> {
+                            System.out.println("exception : " + exception.getMessage());
+                            response.sendRedirect("/login");
+                        }))
+                        .permitAll()
+                );
 
         return http.build();
     }
@@ -30,15 +58,7 @@ public class SecurityConfig {
                 .password("{noop}1111")
                 .roles("USER")
                 .build();
-        UserDetails user2 = User.withUsername("user2")
-                .password("{noop}1111")
-                .roles("USER")
-                .build();
-        UserDetails user3 = User.withUsername("user3")
-                        .password("{noop}1111")
-                        .roles("USER")
-                        .build();
 
-        return new InMemoryUserDetailsManager(user, user2, user3);
+        return new InMemoryUserDetailsManager(user);
     }
 }
