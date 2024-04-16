c0c3425a1343a4f8d8dbeaed6337803eb2099004
moong2 <www.castlehi@gmail.com>
2024-04-16 16:51:48 +0900

    섹션2 <인증 프로세스> 8, 9. 로그아웃 - logout()

--- Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/IndexController.java
+++ Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -44,5 +44,8 @@ public class IndexController {
         return context.getAuthentication().getName();
     }
 
-
+    @GetMapping("/logoutSuccess")
+    public String logoutSuccess() {
+        return "logoutSuccess";
+    }
 }
--- Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -3,6 +3,7 @@ package io.security.springsecuritymaster;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,12 +11,16 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @Configuration
@@ -26,13 +31,36 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/anonymous").hasRole("GUEST")
-                        .requestMatchers("/anonymousContext", "/authentication").permitAll()
+                        .requestMatchers("/logoutSuccess").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
-                .anonymous(anonymous -> anonymous
-                        .principal("guest")
-                        .authorities("ROLE_GUEST")
+//                .csrf(csrf -> csrf.disable())
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "POST"))
+                        .logoutSuccessUrl("/logoutSuccess")
+                        .logoutSuccessHandler(new LogoutSuccessHandler() {
+                            @Override
+                            public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response,
+                                                        Authentication authentication)
+                                    throws IOException, ServletException {
+                                response.sendRedirect("/logoutSuccess");
+                            }
+                        })
+                        .deleteCookies("JSESSIONID", "remember-me")
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .addLogoutHandler(new LogoutHandler() {
+                            @Override
+                            public void logout(HttpServletRequest request, HttpServletResponse response,
+                                               Authentication authentication) {
+                                HttpSession session = request.getSession();
+                                session.invalidate();
+                                SecurityContextHolder.getContextHolderStrategy().getContext().setAuthentication(null);
+                                SecurityContextHolder.getContextHolderStrategy().clearContext();
+                            }
+                        })
+                        .permitAll()
                 );
 
         return http.build();
