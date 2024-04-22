8d6d1f707935824a92985f6a1b1cd66c2ca7b324
moong2 <www.castlehi@gmail.com>
2024-04-22 18:48:48 +0900

    섹션5 <세션 관리> 2. 세션 고정 보호 - sessionManagement().sessionFixation()

--- Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -22,14 +22,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/invalidSessionUrl", "/expiredUrl").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .sessionManagement(session -> session
-//                        .invalidSessionUrl("/invalidSessionUrl")
-                        .maximumSessions(1)
-                        .maxSessionsPreventsLogin(false)
-                        .expiredUrl("/expiredUrl")
+                        .sessionFixation(sessionFixation -> sessionFixation.changeSessionId())
                 )
         ;
 
