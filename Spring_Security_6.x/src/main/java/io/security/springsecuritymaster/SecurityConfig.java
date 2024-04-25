0086eba3d009125c004e32622ad1471efa20448f
moong2 <www.castlehi@gmail.com>
2024-04-25 16:41:09 +0900

    섹션8 <인가 프로세스> 4. 요청 기반 권한 부여 - HttpSecurity.securityMatcher()

--- Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -1,8 +1,8 @@
 package io.security.springsecuritymaster;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,50 +11,16 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
-//
-//        http
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .requestMatchers("/user/{name}").access(new WebExpressionAuthorizationManager("#name == authentication.name"))
-//                        .requestMatchers("/admin/db").access(new WebExpressionAuthorizationManager("hasAuthority('ROLE_DB') or hasAuthority('ROLE_ADMIN')"))
-//                        .anyRequest().authenticated())
-//                .formLogin(Customizer.withDefaults())
-//        ;
-//
-//        return http.build();
-//    }
-
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http, ApplicationContext context) throws Exception {
-//
-//        DefaultHttpSecurityExpressionHandler expressionHandler = new DefaultHttpSecurityExpressionHandler();
-//        expressionHandler.setApplicationContext(context);
-//
-//        WebExpressionAuthorizationManager authorizationManager = new WebExpressionAuthorizationManager("@customWebSecurity.check(authentication, request)");
-//        authorizationManager.setExpressionHandler(expressionHandler);
-//
-//        http
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .requestMatchers("/custom/**").access(authorizationManager)
-//                        .anyRequest().authenticated())
-//                .formLogin(Customizer.withDefaults())
-//        ;
-//
-//        return http.build();
-//    }
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, ApplicationContext context) throws Exception {
-
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(new CustomRequestMatcher("/admin")).hasAuthority("ROLE_ADMIN")
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
         ;
@@ -62,6 +28,18 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    @Order(1)
+    public SecurityFilterChain securityFilterChain2(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+        http
+                .securityMatchers(matchers -> matchers.requestMatchers("/api/**", "/oauth/**"))
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+        ;
+
+        return http.build();
+    }
 
     @Bean
     public UserDetailsService userDetailsService(){
