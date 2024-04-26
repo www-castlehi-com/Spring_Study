24dad519cca99319fa895a0f4b988f9578cc440e
moong2 <www.castlehi@gmail.com>
2024-04-26 21:52:21 +0900

    섹션9 <인가 아키텍처> 7, 8. 메서드 기반 인가 관리자 - PreAuthorizeAuthorizationManager

---
+++ Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/DataService.java
@@ -0,0 +1,23 @@
+package io.security.springsecuritymaster;
+
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataService {
+
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public String getUser() {
+        return "user";
+    }
+
+    @PostAuthorize("returnObject.owner == authentication.name")
+    public Account getOwner(String name) {
+        return new Account(name, false);
+    }
+
+    public String display() {
+        return "display";
+    }
+}
--- Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -1,26 +1,19 @@
 package io.security.springsecuritymaster;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authorization.AuthenticatedAuthorizationManager;
-import org.springframework.security.authorization.AuthorityAuthorizationManager;
-import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.security.web.util.matcher.AnyRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcherEntry;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @EnableWebSecurity
@@ -29,42 +22,21 @@ import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector)
+            throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().access(authorizationManager(null)))
+                        .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
         ;
 
         return http.build();
-    }
-
-    @Bean
-    public AuthorizationManager<RequestAuthorizationContext> authorizationManager(HandlerMappingIntrospector introspector) {
-        List<RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>>> mappings = new ArrayList<>();
-
-        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry1 = new RequestMatcherEntry<>(
-                new MvcRequestMatcher(introspector, "/user"),
-                AuthorityAuthorizationManager.hasAuthority("ROLE_USER"));
-
-        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry2 = new RequestMatcherEntry<>(
-                new MvcRequestMatcher(introspector, "/db"),
-                AuthorityAuthorizationManager.hasAuthority("ROLE_DB"));
-
-        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry3 = new RequestMatcherEntry<>(
-                new MvcRequestMatcher(introspector, "/admin"),
-                AuthorityAuthorizationManager.hasAuthority("ROLE_ADMIN"));
-
-        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry4 = new RequestMatcherEntry<>(
-                AnyRequestMatcher.INSTANCE, new AuthenticatedAuthorizationManager<>());
-
-        mappings.add(requestMatcherEntry1);
-        mappings.add(requestMatcherEntry2);
-        mappings.add(requestMatcherEntry3);
-        mappings.add(requestMatcherEntry4);
-
-        return new CustomRequestMatcherDelegatingAuthorizationManager(mappings);
     }
 
     @Bean
