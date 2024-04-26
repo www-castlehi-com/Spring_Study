15999641345ba4e1f38cad72cca0f3edd349f2e2
moong2 <www.castlehi@gmail.com>
2024-04-26 13:43:04 +0900

    섹션8 <인가 프로세스> 9. 계층적 권한 - RoleHierarchy

--- Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/IndexController.java
+++ Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -6,8 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
-    @GetMapping("/")
-    public String index(){
-        return "index";
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/db")
+    public String db() {
+        return "db";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
     }
 }
--- Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -2,6 +2,8 @@ package io.security.springsecuritymaster;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,27 +21,29 @@ import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer() {
-//        return new WebSecurityCustomizer() {
-//            @Override
-//            public void customize(WebSecurity web) {
-//               web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-//            }
-//        };
-//    }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/images/**").permitAll()
+                        .requestMatchers("user").hasRole("USER")
+                        .requestMatchers("db").hasRole("DB")
+                        .requestMatchers("admin").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
         ;
 
         return http.build();
+    }
+
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_DB\n" +
+                "ROLE_DB > ROLE_USER\n" +
+                "ROLE_USER > ROLE_ANONYMOUS");
+
+        return roleHierarchy;
     }
 
     @Bean
