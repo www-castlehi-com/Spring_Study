d617a73bb38471c69e6851e21eb3029d705f94fd
moong2 <www.castlehi@gmail.com>
2024-04-28 02:14:14 +0900

    섹션11 <통합하기> 1. Servlet API 통합 - SecurityContextHolderAwareRequestFilter

--- Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/IndexController.java
+++ Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -1,5 +1,11 @@
 package io.security.springsecuritymaster;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +21,28 @@ public class IndexController {
     public String db(){
         return "db";
     }
+
     @GetMapping("/admin")
     public String admin(){
         return "admin";
+    }
+
+    @GetMapping("/login")
+    public String login(HttpServletRequest request, MemberDto memberDto) throws ServletException {
+        request.login(memberDto.getUsername(), memberDto.getPassword());
+        System.out.println("login is successful");
+        return "login";
+    }
+
+    @GetMapping("/users")
+    public List<MemberDto> users(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        boolean authenticate = request.authenticate(response);
+
+        if (authenticate) {
+            return List.of(new MemberDto("user", "1111"));
+        }
+        return Collections.emptyList();
     }
 
 }
--- Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -1,12 +1,8 @@
 package io.security.springsecuritymaster;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authorization.AuthorizationEventPublisher;
-import org.springframework.security.authorization.SpringAuthorizationEventPublisher;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -36,21 +32,11 @@ public class SecurityConfig {
                         .requestMatchers("/user").hasAuthority("ROLE_USER")
                         .requestMatchers("/db").hasAuthority("ROLE_DB")
                         .requestMatchers("/admin").hasAuthority("ROLE_ADMIN")
-                        .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults())
+                        .anyRequest().permitAll())
+//                .formLogin(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
-    }
-
-//    @Bean
-//    public AuthorizationEventPublisher authorizationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
-//        return new SpringAuthorizationEventPublisher(applicationEventPublisher);
-//    }
-
-    @Bean
-    public AuthorizationEventPublisher myAuthorizationEventPublisher(ApplicationEventPublisher applicationEventPublisher){
-        return new MyAuthorizationEventPublisher(new SpringAuthorizationEventPublisher(applicationEventPublisher), applicationEventPublisher);
     }
 
     @Bean
