24af982db8d078294d7b8ade90f34f819b0de907
www-castlehi-com <www.castlehi@gmail.com>
2024-04-29 16:50:16 +0900

    섹션 13 <실전 프로젝트 - 비동기 인증> 12. Rest 인증 보안 및 화면 구성

---
+++ Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/users/controller/RestLoginController.java
@@ -0,0 +1,12 @@
+package io.security.springsecuritymaster.users.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestLoginController {
+    @PostMapping("/api/login")
+    public String restLogin() {
+        return "restLogin";
+    }
+}
