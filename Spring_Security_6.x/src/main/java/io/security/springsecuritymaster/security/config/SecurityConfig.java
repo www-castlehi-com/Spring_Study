bd03d28169be722b8cd78097266c019801555767
seongha_park <seongha_park@tmaxsoft.co.kr>
2024-04-29 14:52:49 +0900

    섹션 13 <실전 프로젝트 - 회원 인증 시스템> 8. 커스텀 인증상세 구현하기 - WebAuthenticationDetails / AuthenticationDetailsSource

--- Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
+++ Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package io.security.springsecuritymaster.security.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
 @EnableWebSecurity
 @Configuration
@@ -14,6 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final AuthenticationProvider authenticationProvider;
+    private final AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> authenticationDetailsSource;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -23,7 +27,10 @@ public class SecurityConfig {
                         .requestMatchers("/", "/signup").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(form -> form.loginPage("/login").permitAll())
+                .formLogin(form -> form
+                        .loginPage("/login").permitAll()
+                        .authenticationDetailsSource(authenticationDetailsSource)
+                )
                 .authenticationProvider(authenticationProvider)
         ;
 
