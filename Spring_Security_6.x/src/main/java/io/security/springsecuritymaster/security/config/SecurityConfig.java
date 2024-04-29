0d948f4c7e7fede581980c0235b7f6efc76e18ca
seongha_park <seongha_park@tmaxsoft.co.kr>
2024-04-29 16:50:16 +0900

    섹션 13 <실전 프로젝트 - 비동기 인증> 12. Rest 인증 보안 및 화면 구성

--- Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
+++ Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
@@ -1,14 +1,16 @@
-package io.security.springsecuritymaster.security.configs;
+package io.security.springsecuritymaster.security.config;
 
 import io.security.springsecuritymaster.security.handler.FormAccessDeniedHandler;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -44,6 +46,21 @@ public class SecurityConfig {
                         .permitAll())
                 .authenticationProvider(authenticationProvider)
                 .exceptionHandling(exception -> exception.accessDeniedHandler(new FormAccessDeniedHandler("/denied")))
+        ;
+        return http.build();
+    }
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain restSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .securityMatcher("/api/login")
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon-*").permitAll()
+                        .anyRequest().permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
         ;
         return http.build();
     }
