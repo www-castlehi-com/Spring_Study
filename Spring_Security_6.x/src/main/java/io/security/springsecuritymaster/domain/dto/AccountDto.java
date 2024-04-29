b4d08938fe3b68ac158d7794d4eb46d2f06dca60
seongha_park <seongha_park@tmaxsoft.co.kr>
2024-04-29 12:12:47 +0900

    섹션 13 <실전 프로젝트 - 회원 인증 시스템> 4. 회원가입 / PasswordEncoder

---
+++ Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/domain/dto/AccountDto.java
@@ -0,0 +1,14 @@
+package io.security.springsecuritymaster.domain.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class AccountDto {
+    private Long id;
+    private String username;
+    private String password;
+    private int age;
+    private String roles;
+}
---
+++ Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/users/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package io.security.springsecuritymaster.users.repository;
+
+import io.security.springsecuritymaster.domain.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Account, Long> {
+}
