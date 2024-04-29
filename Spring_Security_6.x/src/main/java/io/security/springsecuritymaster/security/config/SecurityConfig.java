1053b36a9948e1cb560ce27718df8099208558b2
seongha_park <seongha_park@tmaxsoft.co.kr>
2024-04-29 11:32:06 +0900

    섹션 13 <실전 프로젝트 - 회원 인증 시스템> 3. 로그인 페이지 만들기

--- Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
+++ Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
@@ -19,10 +19,11 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon-*").permitAll()
                         .requestMatchers("/").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(Customizer.withDefaults())
+                .formLogin(form -> form.loginPage("/login").permitAll())
         ;
 
         return http.build();
