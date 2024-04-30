3194db7c293b83ff8d6544a3083677b73e2597cd
seongha_park <seongha_park@tmaxsoft.co.kr>
2024-04-30 16:07:12 +0900

    섹션 15 <실전 프로젝트 - 회원 관리 시스템> 21. 회원 관리 시스템 기본 구성

---
+++ Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/admin/repositiory/ResourcesRepository.java
@@ -0,0 +1,14 @@
+package io.security.springsecuritymaster.admin.repositiory;
+
+import io.security.springsecuritymaster.domain.entity.Resources;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ResourcesRepository extends JpaRepository<Resources, Long> {
+    Resources findByResourceNameAndHttpMethod(String resourceName, String httpMethod);
+
+    @Query("select r from Resources r join fetch r.roleSet where r.resourceType = 'url' order by r.orderNum desc")
+    List<Resources> findAllResources();
+}
---
+++ Spring_Security_6.x/src/main/java/io/security/springsecuritymaster/admin/service/RoleService.java
@@ -0,0 +1,14 @@
+package io.security.springsecuritymaster.admin.service;
+
+import io.security.springsecuritymaster.domain.entity.Role;
+
+import java.util.List;
+
+public interface RoleService {
+    Role getRole(Long id);
+    List<Role> getRoles();
+    List<Role> getRolesWithoutExpression();
+    void createRole(Role role);
+
+    void deleteRole(long id);
+}
