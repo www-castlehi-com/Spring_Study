aa326c190c4d512eb8b77b8290ba12aa2dea4879
www-castlehi-com <www.castlehi@gmail.com>
2024-07-13 13:20:28 +0900

    섹션 6 : 스프링 DB 접근 기술 - 순수 JDBC

--- Spring_Introduction/src/main/java/hello/hellospring/SpringConfig.java
+++ Spring_Introduction/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,18 +1,25 @@
 package hello.hellospring;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 
 @Configuration
 public class SpringConfig {
 
+	@Autowired
+	private DataSource dataSource;
+
 	@Bean
 	public MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
+		// return new MemoryMemberRepository();
+		return new JdbcMemberRepository(dataSource);
 	}
 
 	@Bean
