aa1e11c5a820f98f1a2081c6429e8261fcb13dc4
www-castlehi-com <www.castlehi@gmail.com>
2024-07-13 14:46:59 +0900

    섹션 7 : AOP - AOP가 필요한 상황

--- Spring_Introduction/src/main/java/hello/hellospring/service/MemberService.java
+++ Spring_Introduction/src/main/java/hello/hellospring/service/MemberService.java
@@ -21,11 +21,20 @@ public class MemberService {
 	 * 회원 가입
 	 */
 	public Long join(Member member) {
-		validateDuplicateMember(member);    //중복 회원 검증
+		long start = System.currentTimeMillis();
 
-		memberRepository.save(member);
+		try {
+			validateDuplicateMember(member);    //중복 회원 검증
 
-		return member.getId();
+			memberRepository.save(member);
+
+			return member.getId();
+		} finally {
+			long finish = System.currentTimeMillis();
+			long timeMs = finish - start;
+
+			System.out.println("join = " + timeMs + "ms");
+		}
 	}
 
 	private void validateDuplicateMember(Member member) {
