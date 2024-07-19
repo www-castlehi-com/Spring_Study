2b6945b9a0a813078b628abff652750c38445d37
www-castlehi-com <www.castlehi@gmail.com>
2024-07-19 15:20:42 +0900

    Initial Commit

---
+++ Spring_Core_Principles-Basic/src/main/java/hello/spring_core_principlesbasic/SpringCorePrinciplesBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.spring_core_principlesbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCorePrinciplesBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCorePrinciplesBasicApplication.class, args);
+	}
+
+}
---
+++ Spring_Core_Principles-Basic/src/test/java/hello/spring_core_principlesbasic/SpringCorePrinciplesBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.spring_core_principlesbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCorePrinciplesBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
