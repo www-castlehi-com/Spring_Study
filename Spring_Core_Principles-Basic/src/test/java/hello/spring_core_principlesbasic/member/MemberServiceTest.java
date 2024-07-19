6f7957984cd3e469c8234c01fb6960c00523a381
www-castlehi-com <www.castlehi@gmail.com>
2024-07-19 15:48:16 +0900

    섹션 2 : 스프링 핵심 원리 이해1 - 예제 만들기 - 회원 도메인 실행과 테스트

---
+++ Spring_Core_Principles-Basic/src/test/java/hello/spring_core_principlesbasic/member/MemberServiceTest.java
@@ -0,0 +1,22 @@
+package hello.spring_core_principlesbasic.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MemberServiceTest {
+
+	MemberService memberService = new MemberServiceImpl();
+
+	@Test
+	void join() {
+		//given
+		Member member = new Member(1L, "memberA", Grade.VIP);
+
+		//when
+		memberService.join(member);
+		Member findMember = memberService.findMember(1L);
+
+		//then
+		Assertions.assertThat(member).isEqualTo(findMember);
+	}
+}
