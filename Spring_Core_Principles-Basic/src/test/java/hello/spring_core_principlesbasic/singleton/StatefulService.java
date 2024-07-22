aa48a4c6ef4dcc2221c9fec06216dc7499a17b85
www-castlehi-com <www.castlehi@gmail.com>
2024-07-22 22:11:26 +0900

    섹션 5 : 싱글톤 컨테이너 - 싱글톤 방식의 주의점

---
+++ Spring_Core_Principles-Basic/src/test/java/hello/spring_core_principlesbasic/singleton/StatefulService.java
@@ -0,0 +1,16 @@
+package hello.spring_core_principlesbasic.singleton;
+
+public class StatefulService {
+
+	// private int price; // 상태를 유지하는 필드
+
+	public int order(String name, int price) {
+		System.out.println("name = " + name + " price = " + price);
+
+		return price;
+	}
+
+	// public int getPrice() {
+	// 	return price;
+	// }
+}
