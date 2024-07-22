85ec8e98897220a7f52485890f0999810f592408
www-castlehi-com <www.castlehi@gmail.com>
2024-07-22 22:00:20 +0900

    섹션 5 : 싱글톤 컨테이너 - 싱글톤 컨테이너

--- Spring_Core_Principles-Basic/src/test/java/hello/spring_core_principlesbasic/singleton/SingletonTest.java
+++ Spring_Core_Principles-Basic/src/test/java/hello/spring_core_principlesbasic/singleton/SingletonTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import hello.spring_core_principlesbasic.AppConfig;
 import hello.spring_core_principlesbasic.member.MemberService;
@@ -39,5 +40,27 @@ public class SingletonTest {
 		System.out.println("singletonService2 = " + singletonService2);
 
 		assertThat(singletonService1).isSameAs(singletonService2);
+	}
+
+	@Test
+	@DisplayName("스프링 컨테이너와 싱글톤")
+	void springContainer() {
+		// AppConfig appConfig = new AppConfig();
+
+		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(
+			AppConfig.class);
+
+		//1. 조회 : 호출할 때 마다 객체를 생성
+		MemberService memberService1 = ac.getBean(MemberService.class);
+
+		//2. 조회 : 호출할 때 마다 객체를 생성
+		MemberService memberService2 = ac.getBean(MemberService.class);
+
+		//참조값이 다른 것을 확인
+		System.out.println("memberService1 = " + memberService1);
+		System.out.println("memberService2 = " + memberService2);
+
+		// memberService1 != memberService2
+		assertThat(memberService1).isSameAs(memberService2);
 	}
 }
