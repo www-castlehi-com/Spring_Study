5a17c6a7b5337f54e5a7ff879fe918b9a9dcd810
www-castlehi-com <www.castlehi@gmail.com>
2024-07-22 21:52:46 +0900

    섹션 5 : 싱글톤 컨테이너 - 싱글톤 패턴

---
+++ Spring_Core_Principles-Basic/src/test/java/hello/spring_core_principlesbasic/singleton/SingletonService.java
@@ -0,0 +1,16 @@
+package hello.spring_core_principlesbasic.singleton;
+
+public class SingletonService {
+
+	private static final SingletonService instance = new SingletonService();
+
+	private SingletonService() {}
+
+	public static SingletonService getInstance() {
+		return instance;
+	}
+
+	public void login() {
+		System.out.println("싱글톤 객체 로직 호출");
+	}
+}
