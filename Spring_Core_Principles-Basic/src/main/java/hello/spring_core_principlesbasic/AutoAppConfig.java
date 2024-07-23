9acca2d9e47f0b211b854cccb28c8cdc5295c812
www-castlehi-com <www.castlehi@gmail.com>
2024-07-23 21:26:57 +0900

    섹션 6 : 컴포넌트 스캔 - 탐색 위치와 기본 스캔 대상

--- Spring_Core_Principles-Basic/src/main/java/hello/spring_core_principlesbasic/AutoAppConfig.java
+++ Spring_Core_Principles-Basic/src/main/java/hello/spring_core_principlesbasic/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+	// basePackages = "hello.spring_core_principlesbasic.member",
+	// basePackageClasses = AutoAppConfig.class,
 	excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
