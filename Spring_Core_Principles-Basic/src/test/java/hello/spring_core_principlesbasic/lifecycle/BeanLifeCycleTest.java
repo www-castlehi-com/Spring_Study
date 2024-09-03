8d0f6a9aa2707b8c79bc942342a0a08dbc5a6147
www-castlehi-com <www.castlehi@gmail.com>
2024-09-03 20:32:30 +0900

    섹션 9 : 빈 생명주기 콜백 - 빈 생명주기 콜백 시작

---
+++ Spring_Core_Principles-Basic/src/test/java/hello/spring_core_principlesbasic/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,28 @@
+package hello.spring_core_principlesbasic.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+	@Configuration
+	static class LifeCycleConfig {
+
+		@Bean
+		public NetworkClient networkClient() {
+			NetworkClient networkClient = new NetworkClient();
+			networkClient.setUrl("http://hello-spring.dev");
+			return networkClient;
+		}
+	}
+
+	@Test
+	public void LifeCycleTest() {
+		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+
+		NetworkClient client = ac.getBean(NetworkClient.class);
+		ac.close();
+	}
+}
