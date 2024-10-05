68977e5019e9a8b4d4bc06036921fd1e0cecbd01
www-castlehi-com <www.castlehi@gmail.com>
2024-10-05 18:12:28 +0900

    섹션 10 : 빈 스코프 - 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 문제 해결

--- Spring_Core_Principles-Basic/src/test/java/hello/spring_core_principlesbasic/scope/SingletonWithPrototypeTest1.java
+++ Spring_Core_Principles-Basic/src/test/java/hello/spring_core_principlesbasic/scope/SingletonWithPrototypeTest1.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 
 public class SingletonWithPrototypeTest1 {
 
@@ -39,14 +40,12 @@ public class SingletonWithPrototypeTest1 {
 	@Scope("singleton")
 	static class ClientBean {
 
-		private final PrototypeBean prototypeBean;
-
 		@Autowired
-		public ClientBean(PrototypeBean prototypeBean) {
-			this.prototypeBean = prototypeBean;
-		}
+		private Provider<PrototypeBean> prototypeBeanProvider;
 
 		public int logic() {
+			PrototypeBean prototypeBean = prototypeBeanProvider.get();
+
 			prototypeBean.addCount();
 			return prototypeBean.getCount();
 		}
@@ -76,6 +75,6 @@ public class SingletonWithPrototypeTest1 {
 
 		ClientBean clientBean2 = ac.getBean(ClientBean.class);
 		int count2 = clientBean2.logic();
-		assertThat(count2).isEqualTo(2);
+		assertThat(count2).isEqualTo(1);
 	}
 }
