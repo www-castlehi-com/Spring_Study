4ad8e443bdb2f9c75f46b39756fe7c5ae8753b4e
www-castlehi-com <www.castlehi@gmail.com>
2025-01-17 16:09:37 +0900

    섹션 5 : MVC 프레임워크 만들기 - 단순하고 실용적인 컨트롤러 - v4

---
+++ Spring_MVC_1/servlet/src/main/java/hello/servlet/web/frontController/v5/FrontControllerServletV5.java
@@ -0,0 +1,2 @@
+package hello.servlet.web.frontController.v5;public class FrontControllerServletV5 {
+}
---
+++ Spring_MVC_1/servlet/src/main/java/hello/servlet/web/frontController/v5/adapter/ControllerV3HandlerAdapter.java
@@ -0,0 +1,2 @@
+package hello.servlet.web.frontController.v5.adapter;public class ControllerV3HandlerAdapter {
+}
