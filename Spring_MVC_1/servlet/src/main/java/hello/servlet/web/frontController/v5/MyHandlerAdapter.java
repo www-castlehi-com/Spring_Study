417e849f0badf6e98c7141872b088705cd515b48
www-castlehi-com <www.castlehi@gmail.com>
2025-01-17 16:46:22 +0900

    섹션 5 : MVC 프레임워크 만들기 - 유연한 컨트롤러1 - v5

--- Spring_MVC_1/servlet/src/main/java/hello/servlet/web/frontController/v5/MyHandlerAdapter.java
+++ Spring_MVC_1/servlet/src/main/java/hello/servlet/web/frontController/v5/MyHandlerAdapter.java
@@ -1,2 +1,17 @@
-package hello.servlet.web.frontController.v5;public interface MyHandlerAdapter {
+package hello.servlet.web.frontController.v5;
+
+import java.io.IOException;
+
+import hello.servlet.web.frontController.ModelView;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface MyHandlerAdapter {
+
+	boolean supports(Object handler);
+
+	ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws
+		ServletException,
+		IOException;
 }
--- Spring_MVC_1/servlet/src/main/java/hello/servlet/web/frontController/v5/adapter/ControllerV3HandlerAdapter.java
+++ Spring_MVC_1/servlet/src/main/java/hello/servlet/web/frontController/v5/adapter/ControllerV3HandlerAdapter.java
@@ -1,2 +1,38 @@
-package hello.servlet.web.frontController.v5.adapter;public class ControllerV3HandlerAdapter {
+package hello.servlet.web.frontController.v5.adapter;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import hello.servlet.web.frontController.ModelView;
+import hello.servlet.web.frontController.v3.ControllerV3;
+import hello.servlet.web.frontController.v5.MyHandlerAdapter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
+
+	@Override
+	public boolean supports(Object handler) {
+		return (handler instanceof ControllerV3);
+	}
+
+	@Override
+	public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws
+		ServletException, IOException {
+		ControllerV3 controller = (ControllerV3) handler;
+
+		Map<String, String> paramMap = createPramMap(request);
+
+		return controller.process(paramMap);
+	}
+
+	private static Map<String, String> createPramMap(HttpServletRequest request) {
+		Map<String, String> paramMap = new HashMap<>();
+		request.getParameterNames()
+			.asIterator()
+			.forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+		return paramMap;
+	}
 }
