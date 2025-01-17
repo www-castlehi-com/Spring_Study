8ec58a2b855fd45836f90f2d04286a0d12b9f416
www-castlehi-com <www.castlehi@gmail.com>
2025-01-17 15:10:25 +0900

    섹션 5 : MVC 프레임워크 만들기 - 프론트 컨트롤러 도입 - v1

---
+++ Spring_MVC_1/servlet/src/main/java/hello/servlet/web/frontController/v1/controller/MemberSaveControllerV1.java
@@ -0,0 +1,32 @@
+package hello.servlet.web.frontController.v1.controller;
+
+import java.io.IOException;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontController.v1.ControllerV1;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class MemberSaveControllerV1 implements ControllerV1 {
+
+	private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+	@Override
+	public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		int age = Integer.parseInt(request.getParameter("age"));
+
+		Member member = new Member(username, age);
+		memberRepository.save(member);
+
+		// Model에 데이터 보관
+		request.setAttribute("member", member);
+
+		String viewPath = "/WEB-INF/views/save-result.jsp";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+		dispatcher.forward(request, response);
+	}
+}
