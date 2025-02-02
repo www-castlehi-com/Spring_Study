957bd50ec3e9fcd3eb3d6ef5803ffc5db420fa4b
www-castlehi-com <www.castlehi@gmail.com>
2025-02-02 17:37:57 +0900

    섹션 7 : 스프링 MVC 기본 기능 - HTTP 요청 파라미터 쿼리 파라미터, HTML Form

---
+++ Spring_MVC_1/springmvc-basic/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,25 @@
+package hello.springmvc.basic.request;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+
+	@RequestMapping("/request-param-v1")
+	public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String username = request.getParameter("username");
+		int age = Integer.parseInt(request.getParameter("age"));
+
+		log.info("username={}, age={}", username, age);
+
+		response.getWriter().write("ok");
+	}
+}
