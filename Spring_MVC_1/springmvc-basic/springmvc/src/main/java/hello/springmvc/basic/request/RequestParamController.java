521ae50333fb25cfc17e8f13cfacb24aec8d119d
www-castlehi-com <www.castlehi@gmail.com>
2025-02-02 18:07:58 +0900

    섹션 7 : 스프링 MVC 기본 기능 - HTTP 요청 파라미터 @RequestParam

--- Spring_MVC_1/springmvc-basic/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ Spring_MVC_1/springmvc-basic/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,9 +1,13 @@
 package hello.springmvc.basic.request;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -21,5 +25,77 @@ public class RequestParamController {
 		log.info("username={}, age={}", username, age);
 
 		response.getWriter().write("ok");
+	}
+
+	@ResponseBody
+	@RequestMapping("/request-param-v2")
+	public String requestParamV2(@RequestParam("username") String memberName, @RequestParam("age") int memberAge) {
+		log.info("username={}, age={}", memberName, memberAge);
+
+		return "ok";
+	}
+
+	@ResponseBody
+	@RequestMapping("/request-param-v3")
+	public String requestParamV3(@RequestParam String username, @RequestParam int age) {
+		log.info("username={}, age={}", username, age);
+
+		return "ok";
+	}
+
+	@ResponseBody
+	@RequestMapping("/request-param-v4")
+	public String requestParamV4(String username, int age) {
+		log.info("username={}, age={}", username, age);
+
+		return "ok";
+	}
+
+	/**
+	 * @RequestParam.required
+	 * /request-param-required -> username이 없으므로 예외
+	 *
+	 * 주의!
+	 * /request-param-required?username= -> 빈문자로 통과
+	 *
+	 * 주의!
+	 * /request-param-required
+	 * int age -> null을 int에 입력하는 것은 불가능, 따라서 Integer 변경해야 함(또는 다음에 나오는 defaultValue 사용)
+	 */
+	@ResponseBody
+	@RequestMapping("/request-param-required")
+	public String requestParamRequired(@RequestParam(required = true) String username,
+		@RequestParam(required = false) Integer age) {
+		log.info("username={}, age={}", username, age);
+
+		return "ok";
+	}
+
+	/**
+	 * @RequestParam
+	 * - defaultValue 사용 *
+	 * 참고: defaultValue는 빈 문자의 경우에도 적용 * /request-param-default?username=
+	 */
+	@ResponseBody
+	@RequestMapping("/request-param-default")
+	public String requestParamDefault(@RequestParam(required = true, defaultValue = "guest") String username,
+		@RequestParam(required = false, defaultValue = "-1") int age) {
+
+		log.info("username={}, age={}", username, age);
+
+		return "ok";
+	}
+
+	/**
+	 * @RequestParam Map, MultiValueMap
+	 * Map(key=value)
+	 * MultiValueMap(key=[value1, value2, ...]) ex) (key=userIds, value=[id1, id2])
+	 */
+	@ResponseBody
+	@RequestMapping("/request-param-map")
+	public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+		log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+
+		return "ok";
 	}
 }
