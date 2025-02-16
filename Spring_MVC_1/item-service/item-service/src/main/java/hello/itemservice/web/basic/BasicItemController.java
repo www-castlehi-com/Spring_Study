c5eb8ebd75aebc4ab778c0d14473e95f2a6c6456
www-castlehi-com <www.castlehi@gmail.com>
2025-02-16 18:01:40 +0900

    섹션 8 : 스프링 MVC 웹 페이지 만들기 - RedirectAttributes

--- Spring_MVC_1/item-service/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ Spring_MVC_1/item-service/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
@@ -78,11 +79,21 @@ public class BasicItemController {
 		return "basic/item";
 	}
 
-	@PostMapping("/add")
+	// @PostMapping("/add")
 	public String addItemV5(Item item) {
 		itemRepository.save(item);
 
 		return "redirect:/basic/items/" + item.getId();
+	}
+
+	@PostMapping("/add")
+	public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+		Item savedItem = itemRepository.save(item);
+
+		redirectAttributes.addAttribute("itemId", savedItem.getId());
+		redirectAttributes.addAttribute("status", true);
+
+		return "redirect:/basic/items/{itemId}";
 	}
 
 	@GetMapping("/{itemId}/edit")
