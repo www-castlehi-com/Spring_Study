3a78765367c7544dc61d81f6ced3d1699284af8a
www-castlehi-com <www.castlehi@gmail.com>
2025-02-20 20:20:30 +0900

    섹션 2 : 타임리프 기본 기능 - 기본 객체들

--- Spring_MVC_2/thymeleaf/thymeleaf-basic/src/main/java/springmvc2/thymeleafbasic/basic/BasicController.java
+++ Spring_MVC_2/thymeleaf/thymeleaf-basic/src/main/java/springmvc2/thymeleafbasic/basic/BasicController.java
@@ -5,11 +5,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.Data;
 
 @Controller
@@ -50,6 +54,18 @@ public class BasicController {
 		return "basic/variable";
 	}
 
+	@GetMapping("/basic-objects")
+	public String basicObjects(Model model, HttpServletRequest request,
+		HttpServletResponse response, HttpSession session) {
+		session.setAttribute("sessionData", "Hello Session");
+
+		model.addAttribute("request", request);
+		model.addAttribute("response", response);
+		model.addAttribute("servletContext", request.getServletContext());
+
+		return "basic/basic-objects";
+	}
+
 	@Data
 	static class User {
 		private String username;
@@ -58,6 +74,13 @@ public class BasicController {
 		public User(String username, int age) {
 			this.username = username;
 			this.age = age;
+		}
+	}
+
+	@Component("helloBean")
+	static class HelloBean {
+		public String hello(String data) {
+			return "Hello " + data;
 		}
 	}
 }
