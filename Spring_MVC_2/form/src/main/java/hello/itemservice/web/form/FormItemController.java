f7210ab93c1b985c0c93464b390703a84ee5970a
www-castlehi-com <www.castlehi@gmail.com>
2025-03-08 17:43:50 +0900

    섹션 3 : 타임리프 스프링 통합과 폼 - 체크 박스 멀티

--- Spring_MVC_2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ Spring_MVC_2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -1,6 +1,8 @@
 package hello.itemservice.web.form;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,6 +26,16 @@ public class FormItemController {
 
 	private final ItemRepository itemRepository;
 
+	@ModelAttribute("regions")
+	public Map<String, String> regions() {
+		Map<String, String> regions = new LinkedHashMap<>();
+		regions.put("SEOUL", "서울");
+		regions.put("BUSAN", "부산");
+		regions.put("JEJU", "제주");
+
+		return regions;
+	}
+
 	@GetMapping
 	public String items(Model model) {
 		List<Item> items = itemRepository.findAll();
@@ -35,18 +47,21 @@ public class FormItemController {
 	public String item(@PathVariable long itemId, Model model) {
 		Item item = itemRepository.findById(itemId);
 		model.addAttribute("item", item);
+
 		return "form/item";
 	}
 
 	@GetMapping("/add")
 	public String addForm(Model model) {
 		model.addAttribute("item", new Item());
+
 		return "form/addForm";
 	}
 
 	@PostMapping("/add")
 	public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
 		log.info("item.open={}", item.getOpen());
+		log.info("item.regions={}", item.getRegions());
 
 		Item savedItem = itemRepository.save(item);
 		redirectAttributes.addAttribute("itemId", savedItem.getId());
@@ -58,6 +73,7 @@ public class FormItemController {
 	public String editForm(@PathVariable Long itemId, Model model) {
 		Item item = itemRepository.findById(itemId);
 		model.addAttribute("item", item);
+
 		return "form/editForm";
 	}
 
