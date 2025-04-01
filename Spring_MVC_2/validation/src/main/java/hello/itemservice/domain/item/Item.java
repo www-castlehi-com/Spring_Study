5545fd3007b1bf25a6b7fcba268cad6adacf1a37
www-castlehi-com <www.castlehi@gmail.com>
2025-04-01 20:52:25 +0900

    섹션 6 : 검증2 Bean Validation - Bean Validation 한계

--- Spring_MVC_2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ Spring_MVC_2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,6 +12,7 @@ import javax.validation.constraints.NotNull;
 // @ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원 넘게 입력해주세요.")
 public class Item {
 
+    @NotNull    // 수정 요구사항 추가
     private Long id;
     @NotBlank(message = "공백X")
     private String itemName;
@@ -19,7 +20,7 @@ public class Item {
     @Range(min = 1000, max = 1000000)
     private Integer price;
     @NotNull
-    @Max(9999)
+    // @Max(9999)   수정 요구사항 추가
     private Integer quantity;
 
     public Item() {
