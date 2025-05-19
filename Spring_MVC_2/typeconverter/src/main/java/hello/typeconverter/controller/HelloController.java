67f75bb564730e446515a8de80aee6058fe06a74
www-castlehi-com <www.castlehi@gmail.com>
2025-05-19 22:13:33 +0900

    섹션 11 : 스프링 타입 컨버터 - 스프링에 Converter 적용하기

--- Spring_MVC_2/typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
+++ Spring_MVC_2/typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +11,7 @@ public class HelloController {
 
 	@GetMapping("/hello-v1")
 	public String helloV1(HttpServletRequest request) {
-		String data = request.getParameter("data");	// 문자 ㅌ ㅏ입 조회
+		String data = request.getParameter("data");    // 문자 ㅌ ㅏ입 조회
 		Integer intValue = Integer.valueOf(data);// 숫자 타입으로 변경
 
 		System.out.println("intValue = " + intValue);
@@ -19,9 +20,16 @@ public class HelloController {
 	}
 
 	@GetMapping("/hello-v2")
-	public String helloV2(@RequestParam Integer data) {
+	public String helloV2(@RequestParam(name = "data") Integer data) {
 		System.out.println("data = " + data);
 
+		return "ok";
+	}
+
+	@GetMapping("/ip-port")
+	public String ipPort(@RequestParam(name = "ip-port") IpPort ipPort) {
+		System.out.println("ipPort.getIp() = " + ipPort.getIp());
+		System.out.println("ipPort.getPort() = " + ipPort.getPort());
 		return "ok";
 	}
 }
