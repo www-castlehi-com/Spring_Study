be848631240276bd08a84bdd11f95b898f13073a
www-castlehi-com <www.castlehi@gmail.com>
2025-06-03 17:44:45 +0900

    섹션 2 : JDBC 이해 - JDBC 개발 - 등록

---
+++ Spring_DB_1/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -0,0 +1,68 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.connection.DBConnectionUtil;
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * JDBC - DriverManager 사용
+ */
+@Slf4j
+public class MemberRepositoryV0 {
+
+	public Member save(Member member) throws SQLException {
+		String sql = "insert into member(member_id, money) values (?, ?)";
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, member.getMemberId());
+			pstmt.setInt(2, member.getMoney());
+			pstmt.executeUpdate();
+			return member;
+		} catch (SQLException e) {
+			log.error("db error", e);
+			throw e;
+		} finally {
+			close(conn, pstmt, null);
+		}
+	}
+
+	private Connection getConnection() {
+		return DBConnectionUtil.getConnection();
+	}
+
+	private void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				log.error("error", e);
+			}
+		}
+
+		if (pstmt != null) {
+			try {
+				pstmt.close();
+			} catch (SQLException e) {
+				log.error("error", e);
+			}
+		}
+
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				log.error("error", e);
+			}
+		}
+	}
+}
