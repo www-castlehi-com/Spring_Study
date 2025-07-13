365fe20fdfab43364204fca6d86da1eb74e795cc
www-castlehi-com <www.castlehi@gmail.com>
2025-07-13 17:46:16 +0900

    섹션 5 : 데이터 접근 기술 (JPA) - JPA 적용1 : 개발

--- Spring_DB_2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ Spring_DB_2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,6 +1,6 @@
 package hello.itemservice;
 
-import hello.itemservice.config.*;
+import hello.itemservice.config.JpaConfig;
 import hello.itemservice.repository.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -8,16 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
 
 @Slf4j
 // @Import(MemoryConfig.class)
 // @Import(JdbcTemplateV1Config.class)
 // @Import(JdbcTemplateV2Config.class)
 // @Import(JdbcTemplateV3Config.class)
-@Import(MyBatisConfig.class)
+// @Import(MyBatisConfig.class)
+@Import(JpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
