5ff419655a404875ae74a0b2705996271e9c13d4
www-castlehi-com <www.castlehi@gmail.com>
2025-07-17 21:26:59 +0900

    섹션 9 : 데이터 접근 기술 활용 방안 - 실용적인 구조

--- Spring_DB_2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ Spring_DB_2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,6 +1,6 @@
 package hello.itemservice;
 
-import hello.itemservice.config.QuerydslConfig;
+import hello.itemservice.config.V2Config;
 import hello.itemservice.repository.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -17,7 +17,8 @@ import org.springframework.context.annotation.Profile;
 // @Import(MyBatisConfig.class)
 // @Import(JpaConfig.class)
 // @Import(SpringDataJpaConfig.class)
-@Import(QuerydslConfig.class)
+// @Import(QuerydslConfig.class)
+@Import(V2Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
