d05c29ad242c161c5b55098dd3b89b72743e25d4
www-castlehi-com <www.castlehi@gmail.com>
2025-08-03 17:04:33 +0900

    섹션 12. 스프링 트랜잭션 전파1 : 활용 - 단일 트랜잭션

--- Spring_DB_2/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ Spring_DB_2/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -52,8 +52,26 @@ class MemberServiceTest {
 		assertThatThrownBy(() -> memberService.joinV1(username))
 				.isInstanceOf(RuntimeException.class);
 
-		// when : 모든 데이터가 정상 저장된다.
+		//then: 완전히 롤백되지 않고, member 데이터가 남아서 저장된다.
 		assertTrue(memberRepository.find(username).isPresent());
 		assertTrue(logRepository.find(username).isEmpty());
+	}
+
+	/**
+	 * memberService	@Transactional:ON
+	 * memberRepository	@Transactional:OFF
+	 * logRepository	@Transactional:OFF
+	 */
+	@Test
+	void singleTx() {
+		/// given
+		String username = "outerTxOff_success";
+
+		// when
+		memberService.joinV1(username);
+
+		// when : 모든 데이터가 정상 저장된다.
+		assertTrue(memberRepository.find(username).isPresent());
+		assertTrue(logRepository.find(username).isPresent());
 	}
 }
