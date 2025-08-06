b22d39a8f24b4f526a4d666f28980fcce0985cd8
www-castlehi-com <www.castlehi@gmail.com>
2025-08-06 20:49:59 +0900

    섹션 2. 예제 만들기 - 로그 추적기 V2 : 적용

--- Spring_Advanced/advanced/src/main/java/hello/advanced/app/v2/OrderServiceV2.java
+++ Spring_Advanced/advanced/src/main/java/hello/advanced/app/v2/OrderServiceV2.java
@@ -1,23 +1,24 @@
-package hello.advanced.app.v1;
+package hello.advanced.app.v2;
 
+import hello.advanced.trace.TraceId;
 import hello.advanced.trace.TraceStatus;
-import hello.advanced.trace.hellotrace.HelloTraceV1;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class OrderServiceV1 {
+public class OrderServiceV2 {
 
-	private final OrderRepositoryV1 orderRepository;
-	private final HelloTraceV1 trace;
+	private final OrderRepositoryV2 orderRepository;
+	private final HelloTraceV2 trace;
 
-	public void orderItem(String itemId) {
+	public void orderItem(TraceId traceId, String itemId) {
 		TraceStatus status = null;
 		try {
-			status = trace.begin("OrderService.orderItem()");
+			status = trace.beginSync(traceId, "OrderService.orderItem()");
 
-			orderRepository.save(itemId);
+			orderRepository.save(status.getTraceId(), itemId);
 
 			trace.end(status);
 		} catch (Exception e) {
