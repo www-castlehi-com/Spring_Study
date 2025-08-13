99e789999dae16cea641629f29a8c0f241c41f5f
www-castlehi-com <www.castlehi@gmail.com>
2025-08-13 19:02:23 +0900

    섹션 5. 프록시 패턴과 데코레이터 패턴 - 데코레이터 패턴 : 예제 코드2

--- Spring_Advanced/proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ Spring_Advanced/proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -1,6 +1,8 @@
 package hello.proxy.pureproxy.decorator;
 
+import hello.proxy.pureproxy.decorator.code.Component;
 import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import hello.proxy.pureproxy.decorator.code.MessageDecorator;
 import hello.proxy.pureproxy.decorator.code.RealComponent;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,15 @@ public class DecoratorPatternTest {
 	void noDecorator() {
 		RealComponent component = new RealComponent();
 		DecoratorPatternClient client = new DecoratorPatternClient(component);
+		client.execute();
+	}
+
+	@Test
+	void decorator1() {
+		Component component = new RealComponent();
+		Component messageDecorator = new MessageDecorator(component);
+		DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+
 		client.execute();
 	}
 }
