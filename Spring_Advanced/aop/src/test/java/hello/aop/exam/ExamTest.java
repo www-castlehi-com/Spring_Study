023c5ddef9e6aec092ac6f8abee9ab2ef4116591
www-castlehi-com <www.castlehi@gmail.com>
2025-09-04 20:47:24 +0900

    섹션 13. 스프링 AOP 실전 예제 - 로그 출력 AOP

--- Spring_Advanced/aop/src/test/java/hello/aop/exam/ExamTest.java
+++ Spring_Advanced/aop/src/test/java/hello/aop/exam/ExamTest.java
@@ -1,12 +1,15 @@
 package hello.aop.exam;
 
+import hello.aop.exam.aop.TraceAspect;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Import(TraceAspect.class)
 @Slf4j
 @SpringBootTest
 class ExamTest {
