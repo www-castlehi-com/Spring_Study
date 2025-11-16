11f80badd81cd7dfd17c2fba47733cdc5463e404
www-castlehi-com <www.castlehi@gmail.com>
2025-11-16 21:15:34 +0900

    섹션 2 : 수업 사전 준비 - 실습 환경 구축

---
+++ Spring-Reactive-1/reactive/src/test/java/hello/reactive/ReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
