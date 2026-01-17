0faf97067377a95d5ff3bedf611e75ccab7c561d
www-castlehi-com <www.castlehi@gmail.com>
2026-01-17 19:01:21 +0900

    섹션 5. 엔티티 매핑 - 실전 예제 1 : 요구사항 분석과 기본 매핑

---
+++ JPA_Basic/jpabook/src/main/java/jpabook/jpashop/domain/Item.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Item {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "ITEM_ID")
+	private Long id;
+
+	private String name;
+
+	private int price;
+
+	private int stockQuantity;
+}
---
+++ JPA_Basic/jpabook/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "MEMBER_ID")
+	private Long id;
+
+	private String name;
+
+	private String city;
+
+	private String street;
+
+	private String zipcode;
+}
