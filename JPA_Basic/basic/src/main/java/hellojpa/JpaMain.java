4e55e03f6c797cf5bb6609e5534fc14e56a2880b
www-castlehi-com <www.castlehi@gmail.com>
2026-01-29 20:26:16 +0900

    섹션 9. 프록시와 연관관계 처리 - 프록시

--- JPA_Basic/basic/src/main/java/hellojpa/JpaMain.java
+++ JPA_Basic/basic/src/main/java/hellojpa/JpaMain.java
@@ -1,9 +1,7 @@
 package hellojpa;
 
 import jakarta.persistence.*;
-
-import java.time.LocalDateTime;
-import java.util.List;
+import org.hibernate.Hibernate;
 
 public class JpaMain {
 
@@ -18,18 +16,39 @@ public class JpaMain {
 
         try {
             Member member = new Member();
-            member.setUsername("user1");
-            member.setCreatedBy("kim");
-            member.setCreatedDate(LocalDateTime.now());
+            member.setUsername("hello");
             em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            Member reference = em.getReference(Member.class, member.getId());
+            System.out.println("reference.getClass() = " + reference.getClass());
+
+            System.out.println("emf.getPersistenceUnitUtil().isLoaded(reference) = " + emf.getPersistenceUnitUtil().isLoaded(reference));
+            Hibernate.initialize(reference);
+            System.out.println("emf.getPersistenceUnitUtil().isLoaded(reference) = " + emf.getPersistenceUnitUtil().isLoaded(reference));
 
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
+            e.printStackTrace();
         } finally {
             em.close();
         }
 
         emf.close();
+    }
+
+    private static void printMember(Member member) {
+        System.out.println("member = " + member.getUsername());
+    }
+
+    private static void printMemberAndTeam(Member member) {
+        String username = member.getUsername();
+        System.out.println("username = " + username);
+
+        Team team = member.getTeam();
+        System.out.println("team = " + team.getName());
     }
 }
