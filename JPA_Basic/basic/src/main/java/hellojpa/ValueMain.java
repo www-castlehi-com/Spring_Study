01ea48fd266a2744bad34008d92eb295e31fc234
www-castlehi-com <www.castlehi@gmail.com>
2026-02-01 17:55:37 +0900

    섹션 10. 값 타입 - 기본값 타입

---
+++ JPA_Basic/basic/src/main/java/hellojpa/ValueMain.java
@@ -0,0 +1,12 @@
+package hellojpa;
+
+public class ValueMain {
+
+	public static void main(String[] args) {
+		Integer a = 10;
+		Integer b = a;
+
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+	}
+}
