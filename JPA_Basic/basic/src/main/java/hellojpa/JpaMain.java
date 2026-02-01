aa23c2bfe433b41263dfc4f89bdf174019459b8c
www-castlehi-com <www.castlehi@gmail.com>
2026-02-01 18:36:33 +0900

    섹션 10. 값 타입 - 값 타입과 불변 객체

--- JPA_Basic/basic/src/main/java/hellojpa/JpaMain.java
+++ JPA_Basic/basic/src/main/java/hellojpa/JpaMain.java
@@ -19,12 +19,21 @@ public class JpaMain {
 		tx.begin();
 
 		try {
-			Member member = new Member();
-			member.setUsername("hello");
-			member.setHomeAddress(new Address("city", "street", "10"));
-			member.setWorkPeriod(new Period());
+			Address address = new Address("city", "street", "10000");
 
+			Member member = new Member();
+			member.setUsername("member1");
+			member.setHomeAddress(address);
 			em.persist(member);
+
+			Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
+
+			Member member2 = new Member();
+			member2.setUsername("member2");
+			member2.setHomeAddress(copyAddress);
+			em.persist(member2);
+
+			member.getHomeAddress().setCity("newCity");
 
 			tx.commit();
 		} catch (Exception e) {
