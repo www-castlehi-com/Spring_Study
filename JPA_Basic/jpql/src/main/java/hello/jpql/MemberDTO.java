b1bda670ebd3d7124861467924b96488181db386
www-castlehi-com <www.castlehi@gmail.com>
2026-02-21 15:42:43 +0900

    섹션 11. 객체지향 쿼리 언어1 기본 문법 - 프로젝션(SELECT)

---
+++ JPA_Basic/jpql/src/main/java/hello/jpql/MemberDTO.java
@@ -0,0 +1,14 @@
+package hello.jpql;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MemberDTO {
+
+	private String username;
+	private int age;
+}
--- JPA_Basic/jpql/src/main/java/hello/jpql/TestService.java
+++ JPA_Basic/jpql/src/main/java/hello/jpql/TestService.java
@@ -5,6 +5,8 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class TestService {
 
@@ -15,6 +17,16 @@ public class TestService {
 	public void test() {
 		Member member = new Member();
 		member.setUsername("member1");
+		member.setAge(10);
 		em.persist(member);
+
+		em.flush();
+		em.clear();
+
+		List<MemberDTO> result = em.createQuery("select new MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+				.getResultList();
+		MemberDTO memberDTO = result.get(0);
+		System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
+		System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
 	}
 }
