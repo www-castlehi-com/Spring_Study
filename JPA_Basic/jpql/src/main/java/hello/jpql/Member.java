0e2104b289ef74a51a6950bd4d104493099558ae
www-castlehi-com <www.castlehi@gmail.com>
2026-02-21 16:23:11 +0900

    섹션 11. 객체지향 쿼리 언어1 기본 문법 - 조인

--- JPA_Basic/jpql/src/main/java/hello/jpql/Member.java
+++ JPA_Basic/jpql/src/main/java/hello/jpql/Member.java
@@ -17,7 +17,12 @@ public class Member {
 	private String username;
 	private int age;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "TEAM_ID")
 	private Team team;
+
+	public void changeTeam(Team team) {
+		this.team = team;
+		team.getMembers().add(this);
+	}
 }
--- JPA_Basic/jpql/src/main/java/hello/jpql/TestService.java
+++ JPA_Basic/jpql/src/main/java/hello/jpql/TestService.java
@@ -15,24 +15,26 @@ public class TestService {
 
 	@Transactional
 	public void test() {
-		for (int i = 0; i < 100; i++) {
-			Member member = new Member();
-			member.setUsername("member" + i);
-			member.setAge(i);
-			em.persist(member);
-		}
+		Team team = new Team();
+		team.setName("teamA");
+		em.persist(team);
+
+		Member member = new Member();
+		member.setUsername("member");
+		member.setAge(10);
+		member.changeTeam(team);
+
+		em.persist(member);
 
 		em.flush();
 		em.clear();
 
-		List<Member> result = em.createQuery("select m FROM Member m order by m.age desc", Member.class)
-				.setFirstResult(1)
-				.setMaxResults(10)
+		// String query = "select m from Member m inner join m.team t";
+		// String query = "select m from Member m left join m.team t";
+		String query = "select m from Member m, Team t where m.username = t.name";
+		List<Member> result = em.createQuery(query, Member.class)
 				.getResultList();
 
 		System.out.println("result.size() = " + result.size());
-		for (Member member1 : result) {
-			System.out.println("member1 = " + member1);
-		}
 	}
 }
