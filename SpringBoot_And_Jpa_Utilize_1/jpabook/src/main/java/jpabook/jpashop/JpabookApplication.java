9cc950930368613041a2686a4559d051a97479f0
www-castlehi-com <www.castlehi@gmail.com>
2026-02-24 21:07:45 +0900

    섹션 2. 프로젝트 환경설정 - View 환경 설정

--- SpringBoot_And_Jpa_Utilize_1/jpabook/src/main/java/jpabook/jpashop/JpabookApplication.java
+++ SpringBoot_And_Jpa_Utilize_1/jpabook/src/main/java/jpabook/jpashop/JpabookApplication.java
@@ -7,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpabookApplication {
 
 	public static void main(String[] args) {
-		Hello hello = new Hello();
-		hello.setData("hello");
-		String data = hello.getData();
-		System.out.println("data = " + data);
-
 		SpringApplication.run(JpabookApplication.class, args);
 	}
 
