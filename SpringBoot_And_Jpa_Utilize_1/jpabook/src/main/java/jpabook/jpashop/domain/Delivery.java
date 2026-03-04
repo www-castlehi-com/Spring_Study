19a8e295b1124d86d68d49c482a0ef180a44a035
www-castlehi-com <www.castlehi@gmail.com>
2026-03-04 20:34:47 +0900

    섹션 3. 도메인 분석 설계 - 엔티티 설계시 주의점

--- SpringBoot_And_Jpa_Utilize_1/jpabook/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ SpringBoot_And_Jpa_Utilize_1/jpabook/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import static jakarta.persistence.FetchType.*;
+
 @Entity
 @Getter
 @Setter
@@ -14,7 +16,7 @@ public class Delivery {
 	@Column(name = "delivery_id")
 	private Long id;
 
-	@OneToOne(mappedBy = "delivery")
+	@OneToOne(mappedBy = "delivery", fetch = LAZY)
 	private Order order;
 
 	@Embedded
