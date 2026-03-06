baefe9aed06912c6b1dd51c34af2d8634145668c
www-castlehi-com <www.castlehi@gmail.com>
2026-03-06 20:26:22 +0900

    섹션 6. 상품 도메인 개발 - 상품 엔티티 개발(비즈니스 로직 추가)

--- SpringBoot_And_Jpa_Utilize_1/jpabook/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ SpringBoot_And_Jpa_Utilize_1/jpabook/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -28,4 +29,16 @@ public abstract class Item {
 
 	@ManyToMany(mappedBy = "items")
 	private List<Category> categories = new ArrayList<>();
+
+	public void addStock(int quantity) {
+		this.stockQuantity += quantity;
+	}
+
+	public void removeStock(int quantity) {
+		int restStock = this.stockQuantity - quantity;
+		if (restStock < 0) {
+			throw new NotEnoughStockException("need more stock");
+		}
+		this.stockQuantity = restStock;
+	}
 }
