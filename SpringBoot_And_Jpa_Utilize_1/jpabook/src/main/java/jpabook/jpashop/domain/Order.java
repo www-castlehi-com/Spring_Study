b1c6ae711c91b54c574f07d932d6e841e199bc7f
www-castlehi-com <www.castlehi@gmail.com>
2026-03-09 19:37:50 +0900

    섹션 7. 주문 도메인 개발 - 주문 서비스 개발

--- SpringBoot_And_Jpa_Utilize_1/jpabook/src/main/java/jpabook/jpashop/domain/Order.java
+++ SpringBoot_And_Jpa_Utilize_1/jpabook/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
@@ -14,6 +16,7 @@ import static jakarta.persistence.FetchType.*;
 @Table(name = "orders")
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
 	@Id
