34ae8220d41f43bc39980bc09dd3b4fabdc89c76
www-castlehi-com <www.castlehi@gmail.com>
2026-04-14 19:00:12 +0900

    섹션 4. API 개발 고급 - 지연 로딩과 조회 성능 최적화 - 간단한 주문 조회 V2 : 엔티티를 DTO로 변환

--- SpringBoot_And_Jpa_Utilize_2/jpabook/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ SpringBoot_And_Jpa_Utilize_2/jpabook/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,12 +1,16 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -19,9 +23,35 @@ public class OrderSimpleApiController {
 	public List<Order> ordersV1() {
 		List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
 		for (Order order : all) {
-			order.getMember().getName();	// Lazy 강제 초기화
-			order.getDelivery().getAddress();	// Lazy 강제 초기화
+			order.getMember().getName();    // Lazy 강제 초기화
+			order.getDelivery().getAddress();    // Lazy 강제 초기화
 		}
 		return all;
+	}
+
+	@GetMapping("/api/v2/simple-orders")
+	public List<SimpleOrderDto> ordersV2() {
+		List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+		return orders.stream()
+				.map(SimpleOrderDto::new)
+				.toList();
+	}
+
+	@Data
+	static class SimpleOrderDto {
+
+		private Long orderId;
+		private String name;
+		private LocalDateTime orderDate;
+		private OrderStatus orderStatus;
+		private Address address;
+
+		public SimpleOrderDto(Order order) {
+			orderId = order.getId();
+			name = order.getMember().getName();
+			orderDate = order.getOrderDate();
+			orderStatus = order.getStatus();
+			address = order.getDelivery().getAddress();
+		}
 	}
 }
