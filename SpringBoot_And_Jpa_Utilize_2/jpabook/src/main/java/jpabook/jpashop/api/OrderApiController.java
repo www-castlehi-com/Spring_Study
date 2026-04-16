0bafde938e16d4446b07a7407810a7b5f2feda75
www-castlehi-com <www.castlehi@gmail.com>
2026-04-16 20:22:21 +0900

    섹션 5. API 개발 고급 - 컬렉션 조회 최적화 - 주문 조회 V2:엔티티를 DTO로 변환

--- SpringBoot_And_Jpa_Utilize_2/jpabook/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ SpringBoot_And_Jpa_Utilize_2/jpabook/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,13 +1,17 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -27,5 +31,49 @@ public class OrderApiController {
 			orderItems.stream().forEach(o -> o.getItem().getName());
 		}
 		return all;
+	}
+
+	@GetMapping("/api/v2/orders")
+	public List<OrderDto> ordersV2() {
+		List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+		return orders.stream()
+				.map(OrderDto::new)
+				.toList();
+	}
+
+	@Getter
+	static class OrderDto {
+
+		private Long orderId;
+		private String name;
+		private LocalDateTime orderDate;
+		private OrderStatus orderStatus;
+		private Address address;
+		private List<OrderItemDto> orderItems;
+
+		public OrderDto(Order order) {
+			this.orderId = order.getId();
+			this.name = order.getMember().getName();
+			this.orderDate = order.getOrderDate();
+			this.orderStatus = order.getStatus();
+			this.address = order.getDelivery().getAddress();
+			this.orderItems = order.getOrderItems().stream()
+					.map(OrderItemDto::new)
+					.toList();
+		}
+	}
+
+	@Getter
+	static class OrderItemDto {
+
+		private String itemName;
+		private int orderPrice;
+		private int count;
+
+		public OrderItemDto(OrderItem orderItem) {
+			this.itemName = orderItem.getItem().getName();
+			this.orderPrice = orderItem.getOrderPrice();
+			this.count = orderItem.getCount();
+		}
 	}
 }
