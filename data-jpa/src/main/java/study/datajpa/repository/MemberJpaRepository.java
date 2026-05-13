d3601aff61704cbf3db3f9f658126cfd60afa5f6
www-castlehi-com <www.castlehi@gmail.com>
2026-05-13 20:50:23 +0900

    섹션 4. 공통 인터페이스 기능 - 순수 JPA 기반 리포지토리 만들기

--- data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,9 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
 
@@ -18,5 +21,23 @@ public class MemberJpaRepository {
 
 	public Member find(Long id) {
 		return em.find(Member.class, id);
+	}
+
+	public Optional<Member> findById(Long id) {
+		Member member = em.find(Member.class, id);
+		return Optional.ofNullable(member);
+	}
+
+	public List<Member> findAll() {
+		return em.createQuery("select m from Member m", Member.class).getResultList();
+	}
+
+	public long count() {
+		return em.createQuery("select count(m) from Member m", Long.class)
+				.getSingleResult();
+	}
+
+	public void delete(Member member) {
+		em.remove(member);
 	}
 }
