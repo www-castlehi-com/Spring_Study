2af25dd0902d7a48dc61b79a114bae71b7c9bbaf
www-castlehi-com <www.castlehi@gmail.com>
2026-05-13 20:37:21 +0900

    섹션3. 예제 도메인 모델 - 예제 도메인 모델과 동작확인

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,25 +1,44 @@
 package study.datajpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
 	@Id
 	@GeneratedValue
+	@Column(name = "member_id")
 	private Long id;
 
 	private String username;
 
-	protected Member() {
-	}
+	private int age;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "team_id")
+	private Team team;
 
 	public Member(String username) {
 		this.username = username;
+	}
+
+	public Member(String username, int age, Team team) {
+		this.username = username;
+		this.age = age;
+		if (team != null) {
+			changeTeam(team);
+		}
+	}
+
+	public void changeTeam(Team team) {
+		this.team = team;
+		team.getMembers().add(this);
 	}
 }
