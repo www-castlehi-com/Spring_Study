995efd6731a53b59c7f5da1efda8afef941e3ce0
www-castlehi-com <www.castlehi@gmail.com>
2026-05-19 20:54:30 +0900

    섹션 5. 쿼리 메소드 기능 - JPA NamedQuery

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -10,6 +10,9 @@ import lombok.ToString;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id", "username", "age"})
+@NamedQuery(
+		name="Member.findByUsername",
+		query="select m from Member m where m.username = :username")
 public class Member {
 
 	@Id
