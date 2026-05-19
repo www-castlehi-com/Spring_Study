d26791bc02ff4a589db178259a5f8854737f9825
www-castlehi-com <www.castlehi@gmail.com>
2026-05-19 20:39:36 +0900

    섹션 5. 쿼리 메소드 기능 - 메소드 이름으로 쿼리 생성

--- data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -40,4 +40,11 @@ public class MemberJpaRepository {
 	public void delete(Member member) {
 		em.remove(member);
 	}
+
+	public List<Member> findByUsernameAndAgeGreaterThan(String username, int age) {
+		return em.createQuery("select m from Member m where m.username = :username and m.age > :age", Member.class)
+				.setParameter("username", username)
+				.setParameter("age", age)
+				.getResultList();
+	}
 }
