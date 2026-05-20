b54c6b46eb664395a5e8afe84c15be49b7b8c0ea
www-castlehi-com <www.castlehi@gmail.com>
2026-05-20 21:25:26 +0900

    섹션 6. 확장 기능 - 사용자 정의 리포지토리 구현

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -259,4 +259,9 @@ class MemberRepositoryTest {
 		// when
 		List<Member> result = memberRepository.findLockByUsername("member1");
 	}
+
+	@Test
+	public void callCustom() {
+		List<Member> result = memberRepository.findMemberCustom();
+	}
 }
