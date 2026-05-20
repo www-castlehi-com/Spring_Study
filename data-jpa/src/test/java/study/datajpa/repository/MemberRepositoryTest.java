0cd2a77831c615a7c5e1a19ef31020988d669e36
www-castlehi-com <www.castlehi@gmail.com>
2026-05-20 20:38:27 +0900

    섹션 5. 쿼리 메소드 기능 - 벌크성 수정 쿼리

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -185,4 +185,24 @@ class MemberRepositoryTest {
 		assertThat(page.isFirst()).isTrue();
 		assertThat(page.hasNext()).isTrue();
 	}
+
+	@Test
+	public void bulkUpdate() {
+		// given
+		memberRepository.save(new Member("member1", 10));
+		memberRepository.save(new Member("member2", 19));
+		memberRepository.save(new Member("member3", 20));
+		memberRepository.save(new Member("member4", 21));
+		memberRepository.save(new Member("member5", 40));
+
+		// when
+		int resultCount = memberRepository.bulkAgePlus(20);
+
+		List<Member> result = memberRepository.findByUsername("member5");
+		Member member5 = result.get(0);
+		System.out.println("member5 = " + member5);
+
+		// then
+		assertThat(resultCount).isEqualTo(3);
+	}
 }
