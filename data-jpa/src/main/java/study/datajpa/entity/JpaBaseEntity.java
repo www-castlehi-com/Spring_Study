f1315851ab6b261e428be76114940a9ec0eb42f6
www-castlehi-com <www.castlehi@gmail.com>
2026-05-21 20:31:47 +0900

    섹션 6. 확장 기능 - Auditing

---
+++ data-jpa/src/main/java/study/datajpa/entity/JpaBaseEntity.java
@@ -0,0 +1,32 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+public class JpaBaseEntity {
+
+	@Column(updatable = false)
+	private LocalDateTime createdDate;
+	private LocalDateTime updatedDate;
+
+	@PrePersist
+	public void prePersist() {
+		LocalDateTime now = LocalDateTime.now();
+		createdDate = now;
+		updatedDate = now;
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		updatedDate = LocalDateTime.now();
+	}
+}
--- data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
+++ data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
@@ -3,8 +3,10 @@ package study.datajpa.entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.repository.MemberRepository;
 
 import java.util.List;
 
@@ -16,6 +18,8 @@ class MemberTest {
 
 	@PersistenceContext
 	private EntityManager em;
+	@Autowired
+	private MemberRepository memberRepository;
 
 	@Test
 	public void testEntity() {
@@ -42,5 +46,27 @@ class MemberTest {
 			System.out.println("member = " + member);
 			System.out.println("-> member.team = " + member.getTeam());
 		}
+	}
+
+	@Test
+	public void JpaEventBaseEntity() throws InterruptedException {
+		// given
+		Member member = new Member("member1");
+		memberRepository.save(member);
+
+		Thread.sleep(100);
+		member.setUsername("member2");
+
+		em.flush();
+		em.clear();
+
+		// when
+		Member findMember = memberRepository.findById(member.getId()).get();
+
+		// then
+		System.out.println("findMember.createdDate = " + findMember.getCreatedDate());
+		System.out.println("findMember.modifiedDate = " + findMember.getLastModifiedDate());
+		System.out.println("findMember.createdBy = " + findMember.getCreatedBy());
+		System.out.println("findMember.modifiedBy = " + findMember.getLastModifiedBy());
 	}
 }
